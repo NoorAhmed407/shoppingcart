e33de47d374a8942687d82a9326697d09380bc4f
noorahmed407 <engrnoorahmed407@gmail.com>
2020-04-26 09:24:39 -0700

    cards Updated

--- src/components/cards.js
+++ src/components/cards.js
@@ -1,11 +1,14 @@
 import React, {Component} from 'react';
 import './cardstyle.css';
 
-const Card = () =>{
+const Card = (props) =>{
     return(
         <div className="col-md-3">
                 <div class="shadow p-3 bg-white rounded mt-2">
-                    <div className="box p-2"><p> Color of The Years</p></div>
+                    <div className="box p-2">
+                        <p>{props.caption}</p>
+                        <span className="text-right"><img src="https://img.icons8.com/android/24/000000/right.png" alt="icon" /></span>
+                    </div>
                 </div>
         </div>
     );
